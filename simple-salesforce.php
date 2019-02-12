2524ed9e0971efc9d6cb4741b80a0451d235f7a6
alinavalovenko <alina.valovenko@gmail.com>
2019-02-12 16:37:14 +0200

    form was parsed & new values were replaced

--- simple-salesforce.php
+++ simple-salesforce.php
@@ -1,6 +1,7 @@
 <?php
 /*
 	Plugin Name: Simple Salesforce Form Plugin
+	Description: Short-code with salesforce form
 	Version: 1.0
 	Author: Alina Valovenko
 	Author URI: http://www.valovenko.pro
@@ -9,15 +10,19 @@
 
 if ( ! class_exists( 'Simple_Salesforce_Form' ) ) {
 
+	if ( ! defined( 'SSF_TEMP' ) ) {
+		define( 'SSF_TEMP', plugin_dir_path( __FILE__ ) . 'temp' . DIRECTORY_SEPARATOR );
+	}
+
 	class Simple_Salesforce_Form {
 		public function __construct() {
 			register_activation_hook( plugin_basename( __FILE__ ), array( $this, 'ssf_activate' ) );
 			register_deactivation_hook( plugin_basename( __FILE__ ), array( $this, 'ssf_deactivate' ) );
-			register_uninstall_hook( plugin_basename( __FILE__ ), array( $this, 'ssf_uninstall' ) );
+			register_uninstall_hook( plugin_basename( __FILE__ ), 'ssf_uninstall' );
 
 			add_action( 'wp_enqueue_scripts', array( $this, 'ssf_enqueue_scripts' ) );
 
-			add_shortcode( 'salesforce', 'salesforce_form_shortcode_render' );
+			add_shortcode( 'salesforce', array( $this, 'salesforce_form_shortcode_render' ) );
 
 		}
 
@@ -30,7 +35,6 @@ if ( ! class_exists( 'Simple_Salesforce_Form' ) ) {
 		}
 
 		public function ssf_uninstall() {
-			return true;
 		}
 
 		/***
@@ -45,15 +49,60 @@ if ( ! class_exists( 'Simple_Salesforce_Form' ) ) {
 		/***
 		 * [salesforce] shortcode main handler
 		 *
-		 * @param array $atts
+		 * @param $atts
+		 *
+		 * @return string
 		 */
 		function salesforce_form_shortcode_render( $atts ) {
-			$atts = shortcode_atts( array(
+			$output = '';
+			$atts   = shortcode_atts( array(
 				'action' => '#',
-				'oid' => '#',
-				'retUrl'    => '#',
-			), $atts, 'salesforce' );
-			echo 'Test';
+				'oid'    => '#',
+				'returl' => '#',
+			), $atts );
+
+			$form_content = $this->get_salesforce_form_html( $atts['action'], $atts['oid'], $atts['returl'] );
+			ob_start();
+			echo $form_content;
+			$output = ob_get_contents();
+			ob_end_clean();
+
+			return $output;
+		}
+
+		/***
+		 * @param string $action
+		 * @param string $oid
+		 * @param string $retUrl
+		 *
+		 * @return false|string
+		 */
+		public function get_salesforce_form_html( $action = '#', $oid = '#', $retUrl = '#' ) {
+			$action_pattern = '/action=\"(.*?)\"/';
+			$oid_pattern    = '/name="oid"[\s]*value=\"(.*?)\"/';
+			$retUrl_pattern = '/name="retURL"[\s]*value=\"(.*?)\"/';
+
+			$form = file_get_contents( SSF_TEMP . 'form.txt' ) or die( 'Unable to open file ' . SSF_TEMP . 'form.txt' );
+
+			if ( '#' !== $action ) {
+				preg_match_all( $action_pattern, $form, $action_matches );
+				$form = str_replace( $action_matches[1][0], $action, $form );
+			}
+
+			if ( '#' !== $oid ) {
+				preg_match_all( $oid_pattern, $form, $oid_matches );
+				$form = str_replace( $oid_matches[1][0], $oid, $form );
+			}
+
+			if ( '#' !== $retUrl ) {
+				preg_match_all( $retUrl_pattern, $form, $retUrl_matches );
+				$form = str_replace( $retUrl_matches[1][0], $retUrl, $form );
+			}
+
+
+			return $form;
 		}
 	}
+
+	new Simple_Salesforce_Form();
 }
